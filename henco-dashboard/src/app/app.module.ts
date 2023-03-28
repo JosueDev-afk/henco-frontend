9a7b3f4ad115ee7f96df4002261349991c6b4a8d
Josue_Dev <josue_ceballos12@hotmail.com>
2023-03-27 21:35:20 -0600

    paises con mas importaciones

--- henco-dashboard/src/app/app.module.ts
+++ henco-dashboard/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { UserListComponent } from './user-list.component';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaisasMasImportacionesComponent } from './paisas-mas-importaciones/paisas-mas-importaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserListComponent
+    UserListComponent,
+    PaisasMasImportacionesComponent
   ],
   imports: [
     BrowserModule,
